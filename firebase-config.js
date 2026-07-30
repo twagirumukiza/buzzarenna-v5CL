97ed9ee425595175b64396ee955b73728e9899d9
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-07-30 13:32:08 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,15 @@
 // 2) Activez Realtime Database.
 // 3) Collez ici la configuration de votre application Web.
 const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDscz3OZH5pNqK3NwMLa5PNJ3ZENvQMCiw",
+  authDomain: "buzzarena.firebaseapp.com",
+
+  // Remplace cette ligne par l’adresse exacte visible
+  // dans Firebase > Realtime Database > Données
+  databaseURL: "https://buzzarena-default-rtdb.europe-west1.firebasedatabase.app/",
+
+  projectId: "buzzarena",
+  storageBucket: "buzzarena.firebasestorage.app",
+  messagingSenderId: "1070272991513",
+  appId: "1:1070272991513:web:61d59c7aba88b3178bb588"
 };
